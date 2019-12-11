03ac0e8c11ff1801dcf8c25b6f619a270ff5f30b
jamiejamiebobamie <jmccrory@vt.edu>
2019-12-11 00:49:09 -0800

    pushing

--- fire_image_classification.py
+++ fire_image_classification.py
@@ -1,31 +1,39 @@
 import glob
 import pandas as pd
 import numpy as np
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-import keras
-from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import img_to_array
+from sklearn.externals import joblib
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from PIL import Image
 
-files = glob.glob("Fire-Detection-Image-Dataset/Fire images/*")
+
+# 111 images
+files = glob.glob("Fire-Detection-Image-Dataset/Fire_images/*")
 ls_fire = []
 for i in files:
-    ls_fire.append(['Fire-Detection-Image-Dataset/Fire images', i.split("/")[-1], '1'])
+    ls_fire.append(['Fire-Detection-Image-Dataset/Fire_images',
+        i.split("/")[-1], '1'])
 
-files = glob.glob("Fire-Detection-Image-Dataset/No_Fire images/*")
+# 542 images
+files = glob.glob("Fire-Detection-Image-Dataset/No_Fire_images/*")
 ls_no_fire = []
 for i in files:
-    ls_no_fire.append(['Fire-Detection-Image-Dataset/No_Fire images',
+    ls_no_fire.append(['Fire-Detection-Image-Dataset/No_Fire_images',
         i.split("/")[-1], '0'])
 
-# there are 4.88x more no_fire images than fire images.
+# there are 4.88x more "no_fire" images than "fire" images.
 df_fire = pd.DataFrame(ls_fire, columns=['Folder', 'filename', 'label'])
 df_no_fire = pd.DataFrame(ls_no_fire, columns=['Folder', 'filename', 'label'])
 
 # if we perform a very simple data augmentation of rotating the fire images 90
-# degrees four times we'll have 333 more images for a total of 444 fire images.
-# and 981 total images. a more balanced dataset :)
+# degrees four times we'll have a total of 444 fire images and 981 total images.
+# a more balanced dataset :).
 target_dataframe_size = len(df_fire)*4 + len(df_no_fire)
 target_dataframe_size = target_dataframe_size
 
@@ -33,16 +41,18 @@ frames = [df_fire, df_no_fire]
 
 df_total = pd.concat(frames)
 
+# split the data into train and test
+train, test = train_test_split(df_total, test_size=0.2)
+
 input_shape = (128, 128, 3)
 num_classes = 1
 batch_size = 9
-epochs = 5
+epochs = 3
 
 def data_gen(df, batch_size):
     while True:
         x_batch = np.zeros((batch_size, 128, 128, 3))
         y_batch = np.zeros((batch_size, 1))
-        # for j in range(int(len(df)/batch_size)):
         for j in range(int(target_dataframe_size/batch_size)):
             b = 0
             for m, k, n in zip(df['filename'].values[j*batch_size:(j+1)*batch_size],
@@ -61,6 +71,7 @@ def data_gen(df, batch_size):
                 if k == '1':
                     y_batch[b] = 1.0
                     b += 1
+                    # image augmentation
                     for _ in range(3):
                         if b == batch_size:
                             b = 0
@@ -72,7 +83,6 @@ def data_gen(df, batch_size):
                 else:
                     y_batch[b] = 0.0
                     b += 1
-                # print(b)
             yield (x_batch, y_batch)
 
 # https://www.geeksforgeeks.org/rotate-matrix-90-degree-without-using-extra-space-set-2/
@@ -105,38 +115,6 @@ def rotate90(arr):
             j += 1
             k -= 1
 
-# sample code for data augmentation from the class repo.
-# I couldn't figure out how to use the generators alongside or in place of the
-# current generator so I decided to "write" custom code for data augmentation.
-# - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# train_datagen = ImageDataGenerator(
-#         rescale=1./255,
-#         shear_range=0.2,
-#         zoom_range=0.2,
-#         horizontal_flip=True)
-#
-# test_datagen = ImageDataGenerator(rescale=1./255)
-#
-# train_generator = train_datagen.flow_from_directory(
-#         'data/train',
-#         target_size=(150, 150),
-#         batch_size=32,
-#         class_mode='binary')
-#
-# validation_generator = test_datagen.flow_from_directory(
-#         'data/validation',
-#         target_size=(150, 150),
-#         batch_size=32,
-#         class_mode='binary')
-#
-# model.fit_generator(
-#         train_generator,
-#         steps_per_epoch=2000,
-#         epochs=50,
-#         validation_data=validation_generator,
-#         validation_steps=800)
-# - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # building the model.
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -151,15 +129,30 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='sigmoid'))
 
 # compiling the model.
+optimizer = keras.optimizers.Adadelta()
 model.compile(loss=keras.losses.binary_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+              optimizer=optimizer,
               metrics=['accuracy'])
 
 # fitting the model.
-model.fit_generator(generator=data_gen(df_total, batch_size=batch_size),
-                    steps_per_epoch=len(df_total) // batch_size,
+steps = len(train) // batch_size
+model.fit_generator(generator=data_gen(train, batch_size=batch_size),
+                    steps_per_epoch=steps,
                     epochs=epochs, verbose=1)
 
-# pickling the model.
+steps = len(test) // 1 # 131 is the test dataset's length and is a prime number
+validation_generator = data_gen(test, batch_size=1)
+Y_pred = model.predict_generator(validation_generator, steps)
+y_pred = np.argmax(Y_pred, axis=1)
+predictions = []
+for i, val in enumerate(y_pred):
+    predictions.append([val])
+predictions = np.asarray(predictions)
 
-# testing the model on a single image.
+y_true = test[['label']].to_numpy()
+for arr in y_true:
+    arr[0] = int(arr[0])
+# print(confusion_matrix(y_true, predictions)) # BROKEN
+
+# pickling the model.
+joblib.dump(model, 'fire_no_fire_classifire.pkl')
